38dcf3a12d3cd58d8c4895d2b6c80b99853c6347
ovsij <v.ovsij@yandex.ru>
2024-01-14 21:20:51 +0300

    first commit

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ TOKEN = getenv("BOT_TOKEN")
 # bind localhost only to prevent any external access
 WEB_SERVER_HOST = "0.0.0.0"
 # Port for incoming request from reverse proxy. Should be any available port
-WEB_SERVER_PORT = 8080
+WEB_SERVER_PORT = 443
 
 # Path to webhook route, on which Telegram will send requests
 WEBHOOK_PATH = "/webhook"
