3e42dc14c8532f85e04caad8a945067c3863ea80
ovsij <v.ovsij@yandex.ru>
2024-01-14 21:16:56 +0300

    first commit

--- main.py
+++ main.py
@@ -21,7 +21,7 @@ TOKEN = getenv("BOT_TOKEN")
 
 # Webserver settings
 # bind localhost only to prevent any external access
-WEB_SERVER_HOST = "127.0.0.1"
+WEB_SERVER_HOST = "0.0.0.0"
 # Port for incoming request from reverse proxy. Should be any available port
 WEB_SERVER_PORT = 8080
 
